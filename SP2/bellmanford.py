c6ddfb42a5e3baed4570949bde559a0642f93285
Siddharth Vasudevan <vsiddharth88@yahoo.com>
2017-08-02 00:20:53 -0500

    Add files via upload

---
+++ SP2/bellmanford.py
@@ -0,0 +1,64 @@
+__author__ = "Siddharth Vasudevan"
+
+import sys
+
+class Node(object):
+
+	def __init__(self, name):
+		self.name = name
+		self.visited = False
+		self.predecessor = None
+		self.adjacenciesList = []
+		self.minDistance = sys.maxsize
+		
+class Edge(object):
+
+	def __init__(self, weight, startVertex, targetVertex):
+		self.weight = weight
+		self.startVertex = startVertex
+		self.targetVertex = targetVertex
+		
+class BellmanFord(object):
+
+	HAS_CYCLE = False
+	
+	def calculateShortestPath(self, vertexList, edgeList, startVertex):
+	
+		startVertex.minDistance = 0
+		
+		for i in range(0,len(vertexList)-1):
+			for edge in edgeList:
+			
+				u = edge.startVertex
+				v = edge.targetVertex
+				
+				newDistance = u.minDistance + edge.weight
+				
+				if newDistance < v.minDistance:
+					v.minDistance = newDistance
+					v.predecessor = u
+					
+		for edge in edgeList:
+			if self.hasCycle(edge):
+				# print("Negative cycle detected...")
+				BellmanFord.HAS_CYCLE = True
+				return
+				
+	def hasCycle(self, edge):
+		if (edge.startVertex.minDistance + edge.weight) < edge.targetVertex.minDistance:
+			return True
+		else:
+			return False
+			
+	def getShortestPathTo(self, targetVertex):
+
+		if not BellmanFord.HAS_CYCLE:
+			print("Shortest path exists with value: ", targetVertex.minDistance)
+			
+			node = targetVertex
+			
+			while node is not None:
+				print("%s " % node.name)
+				node = node.predecessor
+		else:
+			print("Negative cycle detected...")
